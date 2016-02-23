5c0625b07a281a822cb7d69c669f8a574516a098
David Stinnette <dastinnette@gmail.com>
2016-02-23 14:26:50 -0700

    Refactor Invoices controller by adding invoices_finder & invoices_random controllers. Greatly refactored routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,13 +4,15 @@ Rails.application.routes.draw do
     namespace :v1, defaults: { format: :json } do
       get "/customers/find", to: "customers_finder#show"
       get "/invoice_items/find", to: "invoice_items_finder#show"
-
+      get "/invoices/find", to: "invoices_finder#show"
 
       get "/customers/find_all", to: "customers_finder#index"
       get "/invoice_items/find_all", to: "invoice_items_finder#index"
+      get "/invoices/find_all", to: "invoices_finder#index"
 
       get "/customers/random", to: "customers_random#show"
       get "/invoice_items/random", to: "invoice_items_random#show"
+      get "/invoices/random", to: "invoices_random#show"
 
       resources :customers,     only: [:index, :show]
       resources :invoice_items, only: [:index, :show]
