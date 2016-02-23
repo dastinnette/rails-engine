db6eb8dc2ce88f5ff79c55ee60deecec1c1927fe
David Stinnette <dastinnette@gmail.com>
2016-02-22 20:26:49 -0700

    Add find/find-all & random methods for items controller & model

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -9,5 +9,22 @@ class Api::V1::ItemsController < ApplicationController
     respond_with Item.find(params[:id])
   end
 
+  def find
+    respond_with Item.find_by(items_params)
+  end
+
+  def find_all
+    respond_with Item.where(items_params)
+  end
+
+  def random
+    respond_with Item.random
+  end
+
+  private
+
+  def items_params
+    params.permit(:id, :name, :created_at, :updated_at)
+  end
 
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,10 @@
 class Item < ActiveRecord::Base
   belongs_to :merchant
-
   has_many :invoice_items
   #has_many :invoices, through: :invoice_items
+
+  def self.random
+    order("RANDOM()").first
+  end
+
 end
