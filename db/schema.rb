b5c1439d71003413c05e715c03cd7fef81524e9c
David Stinnette <dastinnette@gmail.com>
2016-02-22 17:05:50 -0700

    Create item model & migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160222235803) do
+ActiveRecord::Schema.define(version: 20160223000441) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,10 +23,22 @@ ActiveRecord::Schema.define(version: 20160222235803) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "items", force: :cascade do |t|
+    t.string   "name"
+    t.string   "description"
+    t.integer  "unit_price"
+    t.integer  "merchant_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  add_index "items", ["merchant_id"], name: "index_items_on_merchant_id", using: :btree
+
   create_table "merchants", force: :cascade do |t|
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "items", "merchants"
 end
