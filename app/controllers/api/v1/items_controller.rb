012801f0667141d267f7f14ac3e5a1d0eda07dcd
David Stinnette <dastinnette@gmail.com>
2016-02-22 19:42:14 -0700

    Add nested items controller within api/v1 and index & show methods

---
+++ app/controllers/api/v1/items_controller.rb
@@ -0,0 +1,13 @@
+class Api::V1::ItemsController < ApplicationController
+  respond_to :json
+
+  def index
+    respond_with Item.all
+  end
+
+  def show
+    respond_with Item.find(params[:id])
+  end
+
+
+end
