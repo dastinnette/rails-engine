49cca499a37b38fbf07a62315f39df100e09c4f2
David Stinnette <dastinnette@gmail.com>
2016-02-23 11:21:08 -0700

    Refactor routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,20 +2,6 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      resources :merchants, defaults: {format: :json} do
-        collection do
-          get "find"
-          get "find_all"
-          get "random"
-        end
-      end
-      resources :items, defaults: {format: :json} do
-        collection do
-          get "find"
-          get "find_all"
-          get "random"
-        end
-      end
       resources :customers, defaults: {format: :json} do
         collection do
           get "find"
@@ -37,9 +23,23 @@ Rails.application.routes.draw do
           get "random"
         end
       end
-      resources :transactions, defaults: {format: :json} do
+      resources :items, defaults: {format: :json} do
         collection do
           get "find"
+          get "find_all"
+          get "random"
+        end
+      end
+      resources :merchants, defaults: {format: :json} do
+        collection do
+          get "find"
+          get "find_all"
+          get "random"
+        end
+      end
+      resources :transactions, defaults: {format: :json} do
+        collection do
+            get "find"
           get "find_all"
           get "random"
         end
