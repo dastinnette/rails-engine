35206e6148c8511660a4f58d657f779cf1ce61d6
David Stinnette <dastinnette@gmail.com>
2016-02-25 07:25:23 -0700

    Additional customer model tests passing

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -2,6 +2,9 @@ class Customer < ActiveRecord::Base
   has_many :invoices
   has_many :transactions, through: :invoices
 
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+
   def self.random
     order("RANDOM()").first
   end
--- spec/controllers/api/v1/models/customer_spec.rb
+++ spec/controllers/api/v1/models/customer_spec.rb
@@ -16,5 +16,15 @@ RSpec.describe Customer, type: :model do
       expect(customer[:name]).to eq "MacDonald's"
     end
 
+    it "is invalid without a first name" do
+      customer = Customer.new(last_name: "Ondricka")
+      expect(customer).to be_invalid
+    end
+
+    it "is invalid without a last name" do
+      customer = Customer.new(first_name: "Joey")
+      expect(customer).to be_invalid
+    end
+
   end
 end
