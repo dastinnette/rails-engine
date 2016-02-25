29e938616ed121ae75a073a3da8cf03b5bb8fc1c
David Stinnette <dastinnette@gmail.com>
2016-02-24 20:55:02 -0700

    Refactored invoices finder controller

--- app/controllers/api/v1/invoice_items_finder_controller.rb
+++ app/controllers/api/v1/invoice_items_finder_controller.rb
@@ -22,25 +22,3 @@ class Api::V1::InvoiceItemsFinderController < ApplicationController
   end
 
 end
-
-#   def find_all_params
-#     respond_with InvoiceItem.where(id: params[:id]) if params[:id]
-#     respond_with InvoiceItem.where(item_id: params[:item_id]) if params[:item_id]
-#     respond_with InvoiceItem.where(invoice_id: params[:invoice_id]) if params[:invoice_id]
-#     respond_with InvoiceItem.where(quantity: params[:quantity]) if params[:quantity]
-#     respond_with InvoiceItem.where(unit_price: params[:unit_price]) if params[:unit_price]
-#     respond_with InvoiceItem.where(created_at: params[:created_at]) if params[:created_at]
-#     respond_with InvoiceItem.where(updated_at: params[:updated_at]) if params[:updated_at]
-#   end
-#
-#   def find_params
-#     respond_with InvoiceItem.find_by(id: params[:id]) if params[:id]
-#     respond_with InvoiceItem.find_by(item_id: params[:item_id]) if params[:item_id]
-#     respond_with InvoiceItem.find_by(invoice_id: params[:invoice_id]) if params[:invoice_id]
-#     respond_with InvoiceItem.find_by(quantity: params[:quantity]) if params[:quantity]
-#     respond_with InvoiceItem.find_by(unit_price: params[:unit_price]) if params[:unit_price]
-#     respond_with InvoiceItem.find_by(created_at: params[:created_at]) if params[:created_at]
-#     respond_with InvoiceItem.find_by(updated_at: params[:updated_at]) if params[:updated_at]
-#   end
-#
-# end
